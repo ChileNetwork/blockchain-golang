f72bd5f3642a0a43e220bda1fc32564d2b3f4033
Mario Soto <contacto@chilenetwork.cl>
2024-03-11 16:43:38 -0300

    Add files via upload

---
+++ crypto/keys_test.go
@@ -0,0 +1,12 @@
+package crypto
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGeneratePrivateKey(t *testing.T) {
+	privKey := GeneratePrivateKey()
+	assert.Equal(t, len(privKey.Bytes()), privKeyLen)
+}
---
+++ doc.go
@@ -0,0 +1,9 @@
+package main
+
+// BLOCKER
+//
+//EO25519 crypto
+//UTXD model
+//Protobuffer encoding
+//GRPC transport (gossip)
+//POS consensus
